10f1c61678d95670e3c04996135aece8521084f5
Amit Shrigondekar <amit.shrigondekar@wgu.edu>
2021-01-03 17:04:42 -0700

    better verticalorder traversal added

--- src/main/java/com/practice/tree/VerticalOrderTraversal.java
+++ src/main/java/com/practice/tree/VerticalOrderTraversal.java
@@ -6,11 +6,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import com.practice.tree.TreeNode;
 
 public class VerticalOrderTraversal {
 
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
+    public List<List<Integer>> verticalTraversalMine(TreeNode root) {
 
         if (root == null) {
             return Collections.emptyList();
@@ -84,5 +83,43 @@ public class VerticalOrderTraversal {
             result.add(temp);
         }
         return result;
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        Map<Integer, List<LevelVal>> map = new TreeMap<>();
+        helper(map, root, 0, 0);
+        List<List<Integer>> res = new ArrayList<>();
+        for (Map.Entry<Integer, List<LevelVal>> entry : map.entrySet()) {
+            List<LevelVal> list = entry.getValue();
+            Collections.sort(list);
+            res.add(list.stream().map(lv -> lv.val).collect(Collectors.toList()));
+        }
+        return res;
+    }
+
+    void helper(Map<Integer, List<LevelVal>> map, TreeNode node, int colNum, int level) {
+        if (node == null) return;
+        map.putIfAbsent(colNum, new ArrayList<LevelVal>());
+        map.get(colNum).add(new LevelVal(level, node.val));
+        helper(map, node.left, colNum - 1, level + 1);
+        helper(map, node.right, colNum + 1, level + 1);
+    }
+}
+
+
+class LevelVal implements Comparable<LevelVal>{
+    int val;
+    int level;
+
+    LevelVal(int level, int val) {
+        this.level = level;
+        this.val = val;
+    }
+
+    @Override
+    public int compareTo(LevelVal other) {
+        int ret = this.level - other.level;
+        if (ret != 0) return ret;
+        return this.val < other.val ? -1 : 1;
     }
 }
