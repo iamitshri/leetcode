a5e4db0c1527021d9521c0379427b0fefd00effc
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-04-19 06:20:01 -0600

    Time: 6 ms (42.47%), Space: 41.9 MB (25.95%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    int count=0;
+    Set<Integer> diag1 = new HashSet<>();
+    Set<Integer> diag2 = new HashSet<>();
+    Set<Integer> colSet = new HashSet<>();
+   
+    public int totalNQueens(int n) {
+        // place a queen if it can not be attacked
+        // check if  row is taken, 
+        // check if col is taken 
+        // check if diagonal is taken
+        // 0,1
+        helper(0,n);
+        return count;
+    }
+      
+    
+    void helper(int row, int n){
+        
+        for(int col=0; col < n;col++){
+            if(colSet.contains(col)){
+                continue;
+            }     
+           
+            if(diag1.contains(row+col)){
+                continue;
+            }
+            if(diag2.contains(row-col)){
+                continue;
+            }
+            if(row==n-1){
+                count++;
+                
+            }else if(row < n){
+                colSet.add(col);
+                diag1.add(row+col);
+                diag2.add(row-col);
+                
+                helper(row+1, n);
+                
+                colSet.remove(col);
+                diag1.remove(row+col);
+                diag2.remove(row-col);
+            }
+        }
+    }
+}
