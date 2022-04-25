b66a6eb722372c69ff7273204cb68b5cddbb7cd0
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-04-25 06:37:05 -0600

    Time: 0 ms (100.00%), Space: 40.2 MB (74.20%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -14,7 +14,60 @@
  * }
  */
 class Solution {
+    
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p==null && q==null)
+            return true;
+        
+        if(p==null || q==null)
+            return false;
+        
+        ArrayDeque<TreeNode> ps = new ArrayDeque();
+        ArrayDeque<TreeNode> qs = new ArrayDeque();
+        ps.add(p);
+        qs.add(q);
+        
+        while(!ps.isEmpty() && !qs.isEmpty()){
+            TreeNode p1 = ps.poll();
+            TreeNode q1 = qs.poll();
+            
+            if(check(p1,q1)){
+                
+                if(!check(p1.left,q1.left)){
+                    return false;
+                }
+                if(p1.left!=null){
+                    ps.add(p1.left);
+                    qs.add(q1.left);
+                }
+                
+                if(!check(p1.right,q1.right)){
+                    return false;
+                }
+                if(p1.right!=null){
+                    ps.add(p1.right);
+                    qs.add(q1.right);
+                }
+            }else{
+               return false; 
+            }
+        }
+        return ps.isEmpty() && qs.isEmpty();
+    }
+    
+    boolean check(TreeNode p, TreeNode q){
+        
+        if(p==null && q==null)
+            return true;
+        
+        if(p==null || q==null)
+            return false;
+        
+         return p.val==q.val;
+    }
+    
+    public boolean isSameTreeRecursive(TreeNode p, TreeNode q) {
         
         if(p==null && q==null)
             return true;
