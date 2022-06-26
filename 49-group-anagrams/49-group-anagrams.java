b23b73533fcab673a553f928648d4d54091681a5
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-06-26 14:41:59 -0600

    Time: 32 ms (21.29%), Space: 60 MB (18.36%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,5 +1,32 @@
 class Solution {
+    
     public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String,List<String>> map = new HashMap<>();
+        for(String s: strs){
+            
+            int [] arr = new int[26];
+            for(char c: s.toCharArray()){
+                arr[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i =0 ; i < 26;i++){
+                sb.append('$');
+                sb.append(arr[i]);
+            }
+            String n = sb.toString();
+            var list = map.getOrDefault(n,new ArrayList<String>());
+            list.add(s);
+            map.put(n,list);           
+        }
+        // List<List<String>> result = new ArrayList<>();
+        // for(String s : map.keySet()){
+        //     result.add(map.get(s));
+        // }
+        return new ArrayList(map.values());
+    }
+    
+    public List<List<String>> groupAnagrams1(String[] strs) {
         
         Map<String,List<String>> map = new HashMap<>();
         for(String s: strs){
