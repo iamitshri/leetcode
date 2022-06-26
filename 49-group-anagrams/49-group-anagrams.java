013031c965aa8b5c039514d137f674b1f0a1e374
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-06-26 14:33:31 -0600

    Time: 7 ms (95.57%), Space: 45.2 MB (95.94%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String,List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char [] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String n = new String(arr);
+            var list = map.getOrDefault(n,new ArrayList<String>());
+            list.add(s);
+            map.put(n,list);           
+        }
+        List<List<String>> result = new ArrayList<>();
+        for(String s : map.keySet()){
+            result.add(map.get(s));
+        }
+        return result;
+    }
+}
