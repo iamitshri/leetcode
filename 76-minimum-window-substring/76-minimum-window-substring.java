c97dc9c2c9de058a3ea5c24ae81d582933152f4b
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-07-04 14:52:32 -0600

    Time: 21 ms (54.66%), Space: 50.4 MB (27.81%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        if(t.length() > s.length()) return "";
+        
+        // pattern map ready 
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
+        
+        String result = "";
+        // create a loop to iterate over chars in s 
+        int windowStart=0;
+        int min=Integer.MAX_VALUE;
+        int minS=0;
+        int minE=0;
+        // distinct keys in map
+        int cnt = map.size();
+        for(int windowEnd=0; windowEnd < s.length();windowEnd++){
+            
+            char rightChar = s.charAt(windowEnd);
+            if(map.containsKey(rightChar)){
+                map.put(rightChar,map.get(rightChar)-1);
+                if(map.get(rightChar)==0)
+                    cnt--;
+            }
+            
+            while(cnt==0){
+                char leftChar = s.charAt(windowStart);
+                if(map.containsKey(leftChar)){
+                    if(map.get(leftChar)==0)
+                        cnt++;
+                    map.put(leftChar,map.get(leftChar)+1);
+                }
+                if((windowEnd-windowStart+1) < min ){
+                    min = windowEnd-windowStart+1;
+                    minS = windowStart;
+                    minE = windowEnd;
+                    result = s.substring(minS,minE+1);
+                }
+                windowStart++;
+            }
+            
+            
+        }
+        return result;
+        
+    }
+}
