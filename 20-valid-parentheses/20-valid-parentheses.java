33c49f621fe788ceabb26c5d840f371c675886a2
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-07-18 13:08:41 -0600

    Time: 4 ms (30.61%), Space: 42.1 MB (50.03%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -19,7 +19,7 @@ class Solution {
                 return false;
             
             else
-                if (!stk.isEmpty())
+                
                     stk.pop();
             
         }
