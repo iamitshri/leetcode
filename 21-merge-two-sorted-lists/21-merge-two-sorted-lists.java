993b7196dd6fa8c759122ba3b8f4cb922be53f2c
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-07-18 14:00:04 -0600

    Time: 0 ms (100.00%), Space: 41.5 MB (96.91%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -9,7 +9,66 @@
  * }
  */
 class Solution {
+    
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1==null && list2==null)
+            return null;
+        
+        if(list1==null)
+            return list2;
+        
+        if(list2==null)
+            return list1;
+        
+        
+        
+        ListNode resultHead=null;
+        ListNode result=null;
+        while(list1!=null && list2!=null){
+            if( list1.val <= list2.val){
+                if(result==null){
+                    result = resultHead = list1;
+                }else{
+                    result.next = list1;    
+                    result = list1;
+                }
+                list1 = list1.next;
+            }else{
+                if(result==null){
+                    result = resultHead = list2;
+                }else{
+                    result.next= list2;
+                    result=list2;
+                }
+                list2 = list2.next;
+            }
+        }
+        
+        while(list1==null && list2!=null){
+               if(result==null){
+                    result = resultHead = list2;
+               }else 
+                    result.next= list2;
+                   
+                result=list2;
+                list2 = list2.next;
+        }
+        
+        while(list2==null && list1!=null){
+            if(result==null){
+                result = resultHead = list1;
+            }else 
+                 result.next= list1;
+                  
+            result=list1;
+            list1 = list1.next;
+        }
+        
+        return resultHead;
+        
+    }
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
         
         if(list1==null && list2==null)
             return null;
