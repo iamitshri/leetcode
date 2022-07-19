8ec3ece336e6b6c7bbee5e1478f471b77746169c
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-07-18 22:35:58 -0600

    Time: 9 ms (39.56%), Space: 46.8 MB (17.70%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,47 +1,47 @@
 class Solution {
-    List<List<String>> list= new ArrayList<>();
+    
+    List<List<String>> result = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for(int i=0; i < n;i++){
-          for(int j=0; j < n;j++){
-            board[i][j]='.';
-          }   
-        }
-        
-        helper(n,0,board);
-        return list;
+        helper(0,n,new ArrayList<>());
+        return result;
     }
     
     Set<Integer> cols = new HashSet<>();
     Set<Integer> diag1 = new HashSet<>();
     Set<Integer> diag2 = new HashSet<>();
     
-    public void helper(int n, int row,char[][] board ) {
+    void helper(int row, int n, List<String> list){
         
+         
         if(row==n){
-            List<String> ll = new ArrayList<>();
-            for(int i=0; i < n;i++){
-              ll.add(new String(board[i]));
-            }   
-            list.add(ll);
+            result.add(new ArrayList<>(list));
             return;
         }
-        for(int col=0; col < n ;col++){
+        
+        for(int col=0; col < n;col++){
             
-
-            if(cols.contains(col) || diag1.contains(row+col) || diag2.contains(row-col)){
+            if(cols.contains(col) || diag1.contains(row+col) || diag2.contains(row-col))
                 continue;
-            }
-                
-            cols.add(col); diag1.add(row+col); diag2.add(row-col);
-            board[row][col]='Q';
             
-            helper(n,row+1,board);
+            char [] columns = new char[n];
+            Arrays.fill(columns,'.');
+            columns[col]='Q';
             
-            // backtrack
-            board[row][col]='.';
-            cols.remove(col); diag1.remove(row+col); diag2.remove(row-col);
+            cols.add(col);
+            diag1.add(row+col);
+            diag2.add(row-col);
+            String x = new String(columns);
+           
+            list.add(x);
+            
+            helper(row+1,n,list);
+            
+            list.remove(list.size()-1);
+            cols.remove(col);
+            diag1.remove(row+col);
+            diag2.remove(row-col);
         }
+        
     }
-    
+     
 }
