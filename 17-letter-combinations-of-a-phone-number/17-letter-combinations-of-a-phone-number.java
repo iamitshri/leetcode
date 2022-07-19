63a9360d140c6785e0ab2c9a04a3a54c467b8aa2
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-07-18 21:57:17 -0600

    Time: 1 ms (95.39%), Space: 42.1 MB (82.25%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -5,22 +5,23 @@ class Solution {
         
         String [][] map = {{},{},{"a","b","c"},{"d","e","f"},{"g","h","i"},{"j","k","l"},{"m","n","o"},{"p","q","r","s"},{"t","u","v"},{"w","x","y","z"}};
         List<String> res = new ArrayList<>();
-        helper(map,digits,"",res,0);
+        helper(map,digits,new StringBuilder(),res,0);
         return res;
     }
     
-    void helper(String [][] map, String digits, String str, List<String> res, int start){
+    void helper(String [][] map, String digits, StringBuilder str, List<String> res, int start){
         
         if(str.length()==digits.length()){
-            res.add(str);
+            res.add(str.toString());
             return;
         }
         
          for(int i=start;i < digits.length();i++){
-             
              int num = digits.charAt(i)-'0';
              for(String c : map[num]){
-                 helper(map,digits,str+c,res, i+1);
+                 str.append(c);
+                 helper(map,digits,str,res, i+1);
+                 str.setLength(str.length()-1);
              }
          }
         
