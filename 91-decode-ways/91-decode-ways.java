8f499858f4a62052406536b236ba06c8e65cb47a
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-08-01 16:17:25 -0600

    Time: 2 ms (68.60%), Space: 42.7 MB (25.34%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,9 +1,29 @@
 class Solution {
     public int numDecodings(String s) {
         
-        Integer [] dp = new Integer[s.length()+1];
-       return recursiveWithMemo(0,s,dp);
+        int [] dp = new int[s.length()+1];
+        
+        if(s.charAt(0)=='0')
+            return 0;
+        
+        dp[0]=1;
+        dp[1]= 1;
+        for(int i=2; i < dp.length ;i++){
+            
+            if(s.charAt(i-1)!='0')
+                dp[i] = dp[i-1];
+            
+            int td = Integer.parseInt(s.substring(i-2,i));
+            if(td>=10 && td<=26){
+                dp[i]+= dp[i-2];
+            }
+        }
+            
+        return dp[s.length()];
+     //  return recursiveWithMemo(0,s,dp);
     }
+    
+     
     
     
     int recursiveWithMemo(int i, String s, Integer [] dp){
