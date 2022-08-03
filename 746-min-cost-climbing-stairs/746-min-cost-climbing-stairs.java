8c5abe15214449c4368b495c0c2ce79686eae730
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-08-03 06:36:46 -0600

    Time: 1 ms (89.83%), Space: 42 MB (90.69%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,9 +1,21 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
-      return helper(0,cost);
+        int[] dp = new int[cost.length+1];
+        
+        dp[0]=0;
+        dp[1]=0;
+        // [ 1,2,3]  
+        //[0, 0,2, 2,1]
+        for(int i=2; i < dp.length; i++){
+            int onestep = dp[i-1] + cost[i-1];
+            int twostep = cost[i-2]+ dp[i-2];
+            dp[i] = Math.min(onestep,twostep);
+        }
+        return dp[cost.length];
     }
     
+    //return helper(0,cost);
       private HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
     int helper(int i, int [] cost){
        
