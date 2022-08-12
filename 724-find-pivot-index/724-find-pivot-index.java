1c1cb60e55be97229a406bc1582530bd73457641
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-08-12 10:01:33 -0600

    Time: 2 ms (79.27%), Space: 53.9 MB (12.46%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+     
+        int [] prefix = new int[nums.length];
+        prefix[0]=0;
+        int prefixSum=0;
+        for(int i =1;i<nums.length;i++){
+             prefixSum += nums[i-1];
+            prefix[i] = prefixSum;
+        }
+        int sum=0;
+        int res=-1;
+        for(int i=nums.length-1;i>=0;i--){
+            
+            if(prefix[i]==sum)
+                res = i;
+            
+            sum += nums[i];
+        }
+        
+        return res;
+    }
+}
