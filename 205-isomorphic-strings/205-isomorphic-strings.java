2a9ad4862a8a85b9f2117d6686dc07370000cb79
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-08-13 12:24:42 -0600

    Time: 11 ms (60.78%), Space: 43.4 MB (28.94%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if(s==null || t==null)
+            return false;
+        
+        if(s.length()!=t.length())
+            return false;
+        
+        char [] src = s.toCharArray();
+        char [] dest = t.toCharArray();
+        
+        Map<Character,Character> map = new HashMap<>();
+        Set<Character> visited = new HashSet<>();
+        // f o o 
+        // b a r 
+        
+        for(int i=0;i < src.length;i++){
+            if(!map.containsKey(src[i])){
+                if(!visited.add(dest[i])){
+                    return false;
+                }
+                map.put(src[i],dest[i]);
+                src[i] = dest[i];
+                
+            }else{
+                src[i] = map.get(src[i]);
+            }
+            
+             if(src[i]!=dest[i])
+                return false;
+        }
+        
+        return new String(src).equals(new String(dest));
+        
+    }
+}
