2b08441ba02cef077a7c5e914f53b378ca6d65ce
iamitshri <26192212+iamitshri@users.noreply.github.com>
2022-09-18 09:29:22 -0600

    Time: 4 ms (25.94%), Space: 44.6 MB (55.51%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,7 +1,35 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
     boolean [] used ;
-    public List<List<Integer>> permute(int[] nums) {
+     
+   public List<List<Integer>> permute(int[] nums) {
+        
+       LinkedList<List<Integer>> perms = new LinkedList<>();
+       perms.add(new ArrayList<>());
+       
+       for(int n : nums){
+           
+           int size = perms.size();
+           for(int j=0; j < size;j++){
+               List<Integer> oldPerm = perms.poll();
+
+               for(int i=0; i <= oldPerm.size();i++){
+                   List<Integer> t = new ArrayList<>(oldPerm);
+                    t.add(i,n);
+
+                   if(t.size()==nums.length)
+                       res.add(t);
+                   else
+                       perms.add(t);
+                   }
+             }
+       }
+       
+       return res;
+    }
+    
+    
+    public List<List<Integer>> permute2(int[] nums) {
         used = new boolean[nums.length];
         helper(nums,new ArrayList<>(),used);
         return res;
